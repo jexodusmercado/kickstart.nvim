511a808911eedbc478003d31b1735b88fff1391b
Exo Mercado <jexodusmercado@gmail.com>
2026-04-08 15:16:13 +0800

    harpoon hotkeys

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -10,7 +10,7 @@ return {
     local mark = require 'harpoon.mark'
     local ui = require 'harpoon.ui'
 
-    vim.keymap.set('n', '<leader>a', mark.add_file)
+    vim.keymap.set('n', '<leader>ha', mark.add_file, { desc = 'Harpoon: Add file' })
     vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu)
   end,
 }
